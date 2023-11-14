3e94c5d50eb2b8f3d9267494f50d23e2e52b81cc
davidparent <david.parent@sjsu.edu>
2023-11-14 14:41:15 -0800

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -13,7 +13,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     wire reset = ! rst_n;
     wire [7:0] led_out;
-    //assign uo_out[7:0] = led_out;
+    assign uo_out[7:0] = A[7:0];
     
 
     // use bidirectionals as outputs
