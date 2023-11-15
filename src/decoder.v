76bc37f2454b3e830ac6470104490942fef5d1c8
davidparent <david.parent@sjsu.edu>
2023-11-15 14:21:15 -0800

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -5,19 +5,19 @@ module decoder (
   output  wire [7:0] V_mem     // Output voltage V (8-bit, signed) 
 );
   
-  //wire reset = ! rst_n;
-  //reg [7:0] A;  
+  wire reset = ! rst_n;
+  reg [7:0] A;  
   assign V_mem =I_syn;
-  //always @(posedge clk  or posedge rst_n) begin
-        // if reset, set counter to 0
-       // if (rst_n) begin
-      //      A <= 0;
-    //    end else begin
-    //        if (A>=8'sd50) begin
-    //            A<=-8'sd20;
-    //        end else begin
-       //         A<=A+I_syn/4+(A/8)*(A/8);
-      //  end
-     //   end
- //   end
+  always @(posedge clk  or posedge rst_n) begin
+         if reset, set counter to 0
+        if (rst_n) begin
+            A <= 0;
+       end else begin
+            if (A>=8'sd50) begin
+                A<=-8'sd20;
+            end else begin
+               A<=A+I_syn/4+(A/8)*(A/8);
+        end
+        end
+   end
 endmodule
