bc9e2075e4b5968fea60b0b34b8a570bec68889f
davidparent <david.parent@sjsu.edu>
2023-11-14 20:03:39 -0800

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -11,40 +11,14 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    
-    wire reset = ! rst_n;
-    wire [7:0] led_out;
-    //assign uo_out[7:0] = A[7:0];
-    
-
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
-
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = A[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [7:0] A;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
+ 
     QIF_8B  neuron (
        .clk(clk),
-       .rst_n(reset),
+       .rst_n(rst_n),
        .I_syn(ui_in),
        .V_mem(uio_out)
      );
 
-    //always @(posedge clk) begin
-        // if reset, set counter to 0
-     //   if (reset) begin
-         //   A <= 0;
-       // end else begin
-      //      if (A>=8'sd50) begin
-       //         A<=-8'sd20;
-       //     end else begin
-                A<=A+ui_in/4+(A/8)*(A/8);
-     //   end
-    //    end
- //   end
+
+    
 endmodule
