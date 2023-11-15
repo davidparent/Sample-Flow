66cc18aa5473bbd450c4d4d25c7e3f03c6c2eed7
davidparent <david.parent@sjsu.edu>
2023-11-15 10:10:53 -0800

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -1,44 +1,23 @@
-
-/*
-      -- 1 --
-     |       |
-     6       2
-     |       |
-      -- 7 --
-     |       |
-     5       3
-     |       |
-      -- 4 --
-*/
-
-module seg7 (
-    input wire [3:0] counter,
-    output reg [6:0] segments
+module decoder (
+  input wire clk,          // Clock input
+  input  wire rst_n,       // Reset input
+  input  wire [7:0] I_syn,     // Input B (8-bit, signed)
+  output  wire [7:0] V_mem     // Output voltage V (8-bit, signed) 
 );
-
-    always @(*) begin
-        case(counter)
-            //                7654321
-            0:  segments = 7'b0111111;
-            1:  segments = 7'b0000110;
-            2:  segments = 7'b1011011;
-            3:  segments = 7'b1001111;
-            4:  segments = 7'b1100110;
-            5:  segments = 7'b1101101;
-            6:  segments = 7'b1111101;
-            7:  segments = 7'b0000111;
-            8:  segments = 7'b1111111;
-            9:  segments = 7'b1101111;
-            10: segments = 7'b1110111;
-            11: segments = 7'b1111100;
-            12: segments = 7'b0111001;
-            13: segments = 7'b1011110;
-            14: segments = 7'b1111001;
-            15: segments = 7'b1110001;
-	    default:
-                segments = 7'b0000000;
-        endcase
-    end
-
+  
+  //wire reset = ! rst_n;
+  //reg [7:0] A;  
+  assign V_mem =I_syn;
+  //always @(posedge clk  or posedge rst_n) begin
+        // if reset, set counter to 0
+       // if (rst_n) begin
+      //      A <= 0;
+    //    end else begin
+    //        if (A>=8'sd50) begin
+    //            A<=-8'sd20;
+    //        end else begin
+       //         A<=A+I_syn/4+(A/8)*(A/8);
+      //  end
+     //   end
+ //   end
 endmodule
-
