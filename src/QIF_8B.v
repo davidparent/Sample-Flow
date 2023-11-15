9d3457e3c73e693c378a4d3b38cd89d5a852320e
davidparent <david.parent@sjsu.edu>
2023-11-14 20:22:16 -0800

    Update QIF_8B.v

--- src/QIF_8B.v
+++ src/QIF_8B.v
@@ -5,18 +5,18 @@ module QIF_8B (
   output  wire [7:0] V_mem     // Output voltage V (8-bit, signed) 
 );
   //wire reset = ! rst_n;
-  reg [7:0] A;  
-  assign V_mem = A;
-  always @(posedge clk  or posedge rst_n) begin
+  //reg [7:0] A;  
+  assign V_mem = I_syn*2;
+  //always @(posedge clk  or posedge rst_n) begin
         // if reset, set counter to 0
-        if (rst_n) begin
-            A <= 0;
-        end else begin
-            if (A>=8'sd50) begin
-                A<=-8'sd20;
-            end else begin
-                A<=A+I_syn/4+(A/8)*(A/8);
-        end
-        end
-    end
+       // if (rst_n) begin
+      //      A <= 0;
+    //    end else begin
+    //        if (A>=8'sd50) begin
+    //            A<=-8'sd20;
+    //        end else begin
+       //         A<=A+I_syn/4+(A/8)*(A/8);
+      //  end
+     //   end
+ //   end
 endmodule
