5c2e791d340e99df843319a9dd58566d814edac0
davidparent <david.parent@sjsu.edu>
2023-11-15 09:24:53 -0800

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -23,7 +23,7 @@ set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.1
 set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.05
 
 # RUN_LINTER, LINTER_INCLUDE_PDK_MODELS - Disabling the linter is not recommended!
-set ::env(RUN_LINTER) 1
+set ::env(RUN_LINTER) 0
 set ::env(LINTER_INCLUDE_PDK_MODELS) 1
 
 # Configuration docs: https://openlane.readthedocs.io/en/latest/reference/configuration.html
